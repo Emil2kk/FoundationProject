c11cb408eee7b5f92c4014dbb1cf23a8235d6441
Emil <emileldar@mail.ru>
2022-02-03 18:18:31 +0400

    portfolioda back-inde az calisma

--- FinalTasks/Task12/task12.py
+++ FinalTasks/Task12/task12.py
@@ -2,8 +2,6 @@
 n = int(input())
 for i in range(n):
     print(i + 1, )
-          
 
-    
     
 
--- Portfolio/app.py
+++ Portfolio/app.py
@@ -1,22 +1,29 @@
-
 from flask import Flask,render_template
-import random
-class Exp:
-    def __init__(self,_year,_title,_shorttext):
-        self.year=_year
-        self.title=_title
-        self.shorttext=_shorttext
-        
-exp=[
-    Exp(
-        '2017',
-        'yazdim',
-        'mence bir gun alinacaq'
-    )
-] 
-
-
+from flask_sqlalchemy import SQLAlchemy
 app=Flask(__name__)
-@app.route('/')
-def home():
-    return render_template('index.html',allexp=exp)
+
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database.db'
+db = SQLAlchemy(app)
+
+class Experience(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    year = db.Column(db.String(50))
+    tittle = db.Column(db.String(50))
+    about = db.Column(db.String(200))
+
+
+
+
+
+
+
+@app.route('/',methods=["GET","POST"])
+def appmain():
+    return render_template('index.html')
+@app.route('/admin',methods=["GET","POST"])
+def admin():
+    return render_template('admin.html')
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:////tmp/database.db'
+db = SQLAlchemy(app)
+
+
