dd7a51f01e10e4d18d94adc10e72d2ff111938ee
Emil <emileldar@mail.ru>
2022-02-04 19:26:11 +0400

    3cu merheleden 2-ci task islendi

---
+++ Finaltasks2/newtask2/app.py
@@ -0,0 +1,18 @@
+
+def FindCity(cityname=):
+    
+    if cityname in olkeler:
+        print(cityname)
+    pass
+
+def FindCountry(countryname):
+  # olke adi daxil edildiyi zaman o olkeye aid olan seherlerin adlarini ekranda gostersin
+  pass
+
+def CityCountMax():
+  # ek cox sehere sahib olan olkeni gostersin
+  pass
+
+def CountAllCities():
+  # butun seherlerin sayini ekranda gostersin
+  pass
---
+++ Finaltasks2/newtask3/app.py
@@ -0,0 +1,6 @@
+import shutil
+
+main = r'C:\Users\Admin\Desktop\python\image.png'
+copied = r'C:\Users\Admin\Desktop\new\image.png'
+
+shutil.copyfile(main, copied)
