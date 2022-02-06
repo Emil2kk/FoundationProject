5b8de3d1f0b427cb7b5154def50367f670641007
Emil <emileldar@mail.ru>
2022-02-06 16:40:36 +0400

    Final tasklarden bezileri hell olundu

--- Finaltasks2/newtask2/app.py
+++ Finaltasks2/newtask2/app.py
@@ -1,18 +1,44 @@
 from re import I
 from countries import olkeler
-
+city=input()
 def FindCity(cityname):
+   Countries = list(olkeler.keys())
+   cities = list(olkeler.values())
+   for x in range(len(Countries)):
+    for y in range(len(cities)):
+      if cityname in cities[y] and x==y:
+       print(f'{cityname} -' ,Countries[x])
+  
+   pass
+
+def FindCountry(countryname):
+   if countryname in olkeler.keys():
+    cities = olkeler.get(countryname) 
+    print(f'{countryname} -' ,cities)
+    pass
+
+def CityCountMax():
+ Countries = list(olkeler.keys())
+ cities = list(olkeler.values())
+ countcity=[]
+ maxcity=0
+ for x in range(len(cities)):
+   countcity.append(len(cities[x])) 
+   maxcity=max(countcity)   
+   maxindex = countcity.index(maxcity) 
+ print(Countries[maxindex])
+ pass
+
+def CountAllCities():
+  count=0
+  city=list(olkeler.values())
+  for x in range(len(olkeler.values())):
+    count += len(city[x])       
+  print(count)
   
   pass
 
-def FindCountry(countryname):
-  # olke adi daxil edildiyi zaman o olkeye aid olan seherlerin adlarini ekranda gostersin
-  pass
-
-def CityCountMax():
-  # ek cox sehere sahib olan olkeni gostersin
-  pass
-
-def CountAllCities():
-  # butun seherlerin sayini ekranda gostersin
-  pass
+FindCity(city)
+FindCountry(city)
+CityCountMax()
+CountAllCities()
--- Finaltasks2/newtask5/newtask5.py
+++ Finaltasks2/newtask5/newtask5.py
@@ -1,15 +1,47 @@
-a=[]
-b=[]
-class Info:
+menu='''
+ qeydiyyatdan kecmek ucun [1] yazin:
+ -istifadeci adiniz:
+ -istifadeci parolu:
+ sisteme daxil olmaq ucun [2] yazin
+ ana menyuya qayitmaq ucun [3] yazin
+sistemden cixmaq ucun [4] yazin
+ 
+'''
+
+print(menu)
+secim = int(input('Seciminizi daxil edin: '))
+users =[]
+passwords=[]
+class User:
     def __init__(self,username,password):
-        self.username=input()
-        self.password=int(input())
-       
-    def register(self):
-        a.append(self.username)
-        b.append(self.password)
-        if self.username in a :
-            print("siz artiq qeydiyyatdan kecmisiz")
-    
-first=Info("salam",23)
-first.register()
+        self.username=username
+        self.password=password
+        users.append(self.username)  
+        passwords.append(self.password)
+        
+    def showuserdata(self):
+        print(f'{self.username} siz artiq qeydiyyatdan kecmisiniz')
+
+while True:
+    if secim == 1:
+        username=input('-Istifadeci adiniz: ')
+        password=input('-Parolunuz: ')
+        user= User(username, password)
+        print('Qeydiyyatdan kecdiniz')
+        
+        secim = int(input('Seciminizi daxil edin: '))     
+    if secim==2:
+        username=input('-Istifadeci adiniz: ')
+        password=input('-Parolunuz: ')
+        user= User(username, password)
+        if user.username in users and user.password in passwords:
+           user.showuserdata()
+        elif user.username not in users :
+           print('Qeydiyyatdan kecmemisiniz!')
+        secim = int(input('Seciminizi daxil edin: '))
+    if secim == 3:
+        print(menu) 
+        secim = int(input('Seciminizi daxil edin: '))
+
+    if secim ==4:
+       break
